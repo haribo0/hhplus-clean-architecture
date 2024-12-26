814212db07047d1bbc0968fa2b9434c4f88da8c5
haribo0 <131761108+haribo0@users.noreply.github.com>
2024-12-27 07:20:56 +0900

    initial commit

---
+++ src/main/java/io/hhplus/architecture/interfaces/lecture/api/LectureController.java
@@ -0,0 +1,13 @@
+package io.hhplus.architecture.interfaces.lecture.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/api/v1/lectures")
+@RequiredArgsConstructor
+public class LectureController {
+
+
+}
